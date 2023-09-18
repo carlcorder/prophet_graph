d28424e2152e018fb7f6ec874581df8d6b97d456
Carl Corder <corder.carl@gmail.com>
2023-09-17 19:48:54 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+dsn = R"C:\Users\corder\OneDrive - Security Benefit Life Insurance Company\Documents\Prophet\NAUS_Workspace\PROPHET.PRW"
+prod_name = "N_EIA1"
---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+import config
+from prophet.graph import ProphetGraph
+
+parser = argparse.ArgumentParser(description="Prophet Graph Generator")
+parser.add_argument("--dsn", help="Connection string in format [host[/port]]:database")
+parser.add_argument("--prod_name", help="Master product name")
+cli_args = parser.parse_args()
+
+
+def main(dsn: str, prod_name: str) -> None:
+    graph = ProphetGraph(dsn=dsn, prod_name=prod_name)
+    print(graph.json)
+
+
+if __name__ == '__main__':
+    # main(**vars(cli_args))
+    main(dsn=config.dsn, prod_name=config.prod_name)
